6d57411e3f295f41d85cba52607fe533d1e8eab5
cssxsh <cssxsh@gmail.com>
2024-10-30 13:10:18 +0800

    feat: WillScriptTool

---
+++ WillScriptTool/Program.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using ATool;
+
+namespace Will
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mode = "";
+            var path = "*.scb";
+            switch (args.Length)
+            {
+                case 1:
+                    _encoding = null;
+                    switch (args[0])
+                    {
+                        case "-e":
+                        case "-i":
+                            mode = args[0];
+                            path = Directory
+                                .EnumerateFiles(".", "*.scb", SearchOption.TopDirectoryOnly)
+                                .DefaultIfEmpty(".scb")
+                                .First();
+                            break;
+                        default:
+                            if (File.Exists(args[0]))
+                            {
+                                mode = "-e";
+                                path = args[0];
+                                break;
+                            }
+
+                            if (Directory.Exists(args[0]))
+                            {
+                                mode = "-i";
+                                path = args[0].TrimEnd('~');
+                            }
+
+                            break;
+                    }
+
+                    break;
+                case 2:
+                    _encoding = null;
+                    mode = args[0];
+                    path = args[1];
+                    break;
+                case 3:
+                    mode = args[0];
+                    path = args[1];
+                    _encoding = Encoding.GetEncoding(args[2]);
+                    break;
+            }
+
+            var scripts = Array.Empty<WillScript>();
+            switch (mode)
+            {
+                case "-e":
+                    _encoding ??= Encoding.GetEncoding("SHIFT-JIS");
+                    Console.WriteLine($"Read {Path.GetFullPath(path)}");
+                    using (var stream = File.OpenRead(path))
+                    using (var reader = new BinaryReader(stream, _jis))
+                    {
+                        scripts = reader.ReadWillScripts();
+                    }
+
+                    Directory.CreateDirectory($"{path}~");
+
+                    foreach (var script in scripts)
+                    {
+                        Console.WriteLine($"Export {script.Name}");
+                        using var writer = File.CreateText($"{path}~/{script.Name}.txt");
+                        for (var i = 0; i < script.Commands.Length; i++)
+                        {
+                            var text = Export(script.Commands[i]);
+                            if (text == null) continue;
+                            writer.WriteLine($">{script.Commands[i][1]:X2}");
+                            writer.WriteLine($"◇{i:D4}◇{text}");
+                            writer.WriteLine($"◆{i:D4}◆{text}");
+                            writer.WriteLine();
+                        }
+                    }
+
+                    break;
+                case "-i":
+                    _encoding ??= Encoding.GetEncoding("GBK");
+                    Console.WriteLine($"Read {Path.GetFullPath(path)}");
+                    using (var stream = File.OpenRead(path))
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        scripts = reader.ReadWillScripts();
+                    }
+
+                    foreach (var script in scripts)
+                    {
+                        if (!File.Exists($"{path}~/{script.Name}.txt")) continue;
+                        if (script.Name == "FlgTbl") continue;
+                        Console.WriteLine($"Import {script.Name}");
+                        var translated = new string[script.Commands.Length];
+                        foreach (var line in File.ReadLines($"{path}~/{script.Name}.txt"))
+                        {
+                            var match = Regex.Match(line, @"◆(\d+)◆(.+)$");
+                            if (!match.Success) continue;
+
+                            var index = int.Parse(match.Groups[1].Value);
+                            var text = match.Groups[2].Value;
+
+                            translated[index] = text;
+                        }
+
+                        for (var i = 0; i < script.Commands.Length; i++)
+                        {
+                            if (translated[i] == null) continue;
+                            script.Commands[i] = Import(script.Commands[i], translated[i]);
+                        }
+                    }
+
+                    var filename =
+                        $"{Path.GetFileNameWithoutExtension(path)}_{_encoding.WebName}{Path.GetExtension(path)}";
+                    Console.WriteLine($"Write {filename}");
+                    using (var stream = File.Create(filename))
+                    using (var writer = new BinaryWriter(stream))
+                    {
+                        writer.WriteWillScripts(scripts);
+                    }
+
+                    break;
+                default:
+                    Array.Resize(ref scripts, 0);
+                    Console.WriteLine("Usage:");
+                    Console.WriteLine("  Export text : WillTool -e [script.dat] [encoding]");
+                    Console.WriteLine("  Import text : WillTool -i [script.dat] [encoding]");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    return;
+            }
+        }
+
+        private static Encoding _encoding = Encoding.Default;
+
+        private static Encoding _jis = Encoding.GetEncoding("SHIFT-JIS");
+
+        private const string FileHead = "ARCG";
+
+        private static WillScript[] ReadWillScripts(this BinaryReader reader)
+        {
+            var head = _encoding.GetString(reader.ReadBytes(4));
+            if (head != FileHead) throw new NotSupportedException($"Not supported version: {head}.");
+            var version = reader.ReadUInt32();
+            if (version != 0x0001_0000u) throw new NotSupportedException($"Not supported version: {version:X8}.");
+            var offset = reader.ReadUInt32(); // index offset
+            _ = reader.ReadInt32(); // index size
+            var folders = reader.ReadUInt16();
+            if (folders != 0x0000_0001u) throw new NotSupportedException($"Not supported folders: {folders}.");
+            var files = reader.ReadUInt32();
+            var scripts = new WillScript[files];
+
+            reader.BaseStream.Position = offset;
+            // var folder = _jis.GetString(reader.ReadBytes(reader.ReadByte() - 1).TrimEnd());
+            // if (folder.Length != 0) throw new NotSupportedException($"Not supported folder: {folder}.");
+            var folder = reader.ReadUInt32();
+            if (folder != 0x0000_0004) throw new NotSupportedException($"Not supported folder: {folder:X8}.");
+            offset = reader.ReadUInt32();
+            _ = reader.ReadUInt32(); // folder files count
+
+            reader.BaseStream.Position = offset;
+            for (var i = 0; i < files; i++)
+            {
+                var len = reader.ReadByte() - 1;
+                var name = _jis.GetString(reader.ReadBytes(len).TrimEnd());
+                offset = reader.ReadUInt32(); // file offset
+                var size = reader.ReadInt32(); // size offset
+
+                var position = reader.BaseStream.Position;
+                reader.BaseStream.Position = offset;
+                var bytes = reader.ReadBytes(size);
+                // Console.WriteLine($"file: {name.ReplaceGbkUnsupported()}");
+
+                scripts[i] = new WillScript(name, bytes);
+                reader.BaseStream.Position = position;
+            }
+
+            return scripts;
+        }
+
+        private static void WriteWillScripts(this BinaryWriter writer, WillScript[] scripts)
+        {
+            
+            writer.Write(_encoding.GetBytes(FileHead));
+            writer.Write(0x0001_0000u);
+            writer.Write(0xFFFF_FFFFu);
+            writer.Write(0xFFFF_FFFFu);
+            writer.Write((ushort)1);
+            writer.Write((uint)scripts.Length);
+
+            writer.BaseStream.Position = 0x20;
+            for (var i = 0; i < scripts.Length; i++)
+            {
+                var bytes = scripts[i].ToBytes();
+                writer.Write(bytes);
+            }
+            
+            var offset = writer.BaseStream.Position;
+            writer.Write(0x0000_0004u);
+            writer.Write((uint)(writer.BaseStream.Position + 0x0C));
+            writer.Write((uint)scripts.Length);
+            writer.Write(0x0000_0000u);
+
+            var position = 0x0000_0020u;
+            for (var i = 0; i < scripts.Length; i++)
+            {
+                var bytes = _jis.GetBytes(scripts[i].Name);
+                var len = (bytes.Length + 2 + 3) & 0xFFFF_FFFCu;
+                Array.Resize(ref bytes, (int)(len - 1));
+                writer.Write((byte)len);
+                writer.Write(bytes);
+                var size = (uint)scripts[i].Commands.Sum(command => command.Length);
+                writer.Write(position);
+                writer.Write(size);
+                position += size;
+            }
+            
+            writer.Write(0x0000_0000u);
+            var diff = writer.BaseStream.Position - offset;
+            writer.BaseStream.Position = 0x0000_0008;
+            writer.Write((uint)offset);
+            writer.Write((uint)diff);
+        }
+
+        private static string Export(byte[] command)
+        {
+            if (command.Length <= 2) return null;
+            // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+            switch (command[1])
+            {
+                case 0x09:
+                case 0x21:
+                case 0x26:
+                {
+                    var offset = 2;
+                    if (command.Length >= 6 && command[0x05] == 0x00)
+                    {
+                        // var value = BitConverter.ToUInt32(command, offset);
+                        // Console.WriteLine($"0x{value:X8}");
+                        offset += 4;
+                    }
+                    else if (command[0x02] == 0x00)
+                    {
+                        // var value = command[offset];
+                        // Console.WriteLine($"0x{value:X2}");
+                        offset += 1;
+                    }
+
+                    var diff = command.Length - offset;
+                    return diff == 0
+                        ? null
+                        : _encoding.GetString(command, offset, diff - 1);
+                }
+                case 0x25:
+                {
+                    var offset = 2;
+                    for (; offset < 8; offset++)
+                    {
+                        if (command[offset] == 0x00) break;
+                    }
+
+                    return _encoding.GetString(command, 2, offset - 2);
+                }
+                default:
+                    return null;
+            }
+        }
+
+        private static byte[] Import(byte[] command, string text)
+        {
+            text = text.ReplaceGbkUnsupported();
+            // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+            switch (command[1])
+            {
+                case 0x09:
+                {
+                    var bytes = _encoding.GetBytes(text);
+                    var offset = 2;
+                    if (command.Length >= 6 && command[0x05] == 0x00)
+                    {
+                        offset += 4;
+                    }
+                    else if (command[0x02] == 0x00)
+                    {
+                        offset += 1;
+                    }
+
+                    Array.Resize(ref command, offset + bytes.Length + 1);
+                    bytes.CopyTo(command, offset);
+                }
+                    break;
+                case 0x25:
+                {
+                    var bytes = _encoding.GetBytes(text);
+                    bytes.CopyTo(command, 2);
+                }
+                    break;
+                default:
+                    // throw new NotSupportedException($"Import {command[1]:X2} is not supported");
+                    break;
+            }
+
+            return command;
+        }
+    }
+}
---
+++ WillScriptTool/WillScript.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ATool;
+
+namespace Will
+{
+    public readonly struct WillScript
+    {
+        public readonly string Name;
+
+        public readonly byte[][] Commands;
+
+        public WillScript(string name, byte[] bytes)
+        {
+            Name = name;
+
+            var commands = new List<byte[]>();
+            // ReSharper disable once RedundantAssignment
+            var temp = Array.Empty<byte>();
+            using (var steam = new MemoryStream(bytes))
+            using (var reader = new BinaryReader(steam))
+            {
+                while (steam.Position < bytes.Length)
+                {
+                    var position = steam.Position;
+                    var size = reader.ReadByte();
+                    if (size == 0x00 || size == 0xFF)
+                    {
+                        steam.Position = position;
+                        temp = reader.ReadBytes((int)(bytes.Length - position));
+                        if (temp.Any(b => b != 0x00) && temp.Any(b => b != 0xFF))
+                            throw new FormatException($"{name} at 0x{position:X8}");
+                        commands.Add(temp);
+                        break;
+                    }
+                    var instruction = reader.ReadByte();
+
+                    switch (instruction)
+                    {
+                        case 0x01:
+                        case 0x02:
+                        case 0x03:
+                        case 0x04:
+                        case 0x05:
+                        case 0x06:
+                        case 0x07:
+                        case 0x08:
+                        case 0x09:
+                        case 0x0A:
+                        case 0x0B:
+                        case 0x0C:
+                        case 0x0D:
+                        case 0x0E:
+                        case 0x21:
+                        case 0x26:
+                        case 0x3E:
+                        {
+                            var offset = 2;
+                            if (size >= 6 && bytes[position + 0x05] == 0x00)
+                            {
+                                var value = reader.ReadUInt32();
+                                // Console.WriteLine($"0x{value:X8}");
+                                offset += 4;
+                            }
+                            else if (bytes[position + 0x02] == 0x00)
+                            {
+                                var value = reader.ReadByte();
+                                // Console.WriteLine($"0x{value:X2}");
+                                offset += 1;
+                            }
+
+                            if (offset == size) break;
+                            temp = reader.ReadBytes(size - offset).TrimEnd();
+                            if (temp.Length != 0 && temp.Length != size - offset - 1)
+                                throw new FormatException($"{name} at 0x{position:X8}");
+                            // Console.WriteLine($"'{encoding.GetString(temp).ReplaceGbkUnsupported()}'");
+                        }
+                            break;
+                        case 0x25:
+                        {
+                            temp = reader.ReadBytes(6);
+                            // Console.WriteLine($"'{encoding.GetString(temp.TrimEnd())}'");
+                        }
+                            break;
+                        case 0x47:
+                        {
+                            temp = reader.ReadBytes(6);
+                            // Console.WriteLine(BitConverter.ToString(temp));
+                            temp = reader.ReadBytes(size - 2 - 6);
+                            // Console.WriteLine($"'{encoding.GetString(temp)}'");
+                        }
+                            break;
+                        case 0x57:
+                        {
+                            if (size == 4)
+                            {
+                                temp = reader.ReadBytes(2);
+                                // Console.WriteLine(BitConverter.ToString(temp));
+                                break;
+                            }
+                            temp = reader.ReadBytes(4);
+                            // Console.WriteLine(BitConverter.ToString(temp));
+                            temp = reader.ReadBytes(size - 2 - 4).TrimEnd();
+                            if (temp.Length != size - 2 - 4 - 1)
+                                throw new FormatException($"{name} at 0x{position:X8}");
+                            // Console.WriteLine($"'{encoding.GetString(temp)}'");
+                        }
+                            break;
+                        case 0x23:
+                        case 0x24:
+                        case 0x2A:
+                        case 0x2E:
+                        case 0x3A:
+                        case 0x40:
+                        case 0x43:
+                        case 0x46:
+                        case 0x4D:
+                        case 0x52:
+                        case 0x7E:
+                        {
+                            temp = reader.ReadBytes(size - 2);
+                            // Console.WriteLine(BitConverter.ToString(temp));
+                        }
+                            break;
+                        default:
+                        {
+                            temp = reader.ReadBytes(size - 2);
+                            // Console.WriteLine(BitConverter.ToString(temp));
+                        }
+                            throw new FormatException($"{name} with 0x{instruction:X2}");
+                    }
+
+                    steam.Position = position;
+                    commands.Add(reader.ReadBytes(size));
+                }
+            }
+
+            Commands = commands.ToArray();
+        }
+
+        public byte[] ToBytes()
+        {
+            var bytes = new byte[Commands.Sum(command => command.Length)];
+            var index = 0;
+            foreach (var command in Commands)
+            {
+                command.CopyTo(bytes, index);
+                index += command.Length;
+            }
+            
+            return bytes;
+        }
+    }
+}
