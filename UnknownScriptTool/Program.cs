9b60da0d5144849d21765143fbe3e0f50e93ecce
cssxsh <cssxsh@gmail.com>
2024-11-25 23:08:09 +0800

    feat: UnknownScriptTool

---
+++ UnknownScriptTool/Program.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using ATool;
+
+namespace Unknown
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mode = "";
+            var path = "scenario.aqa";
+            switch (args.Length)
+            {
+                case 1:
+                    _encoding = null;
+                    switch (args[0])
+                    {
+                        case "-e":
+                        case "-i":
+                            mode = args[0];
+                            break;
+                        default:
+                            if (File.Exists(args[0]))
+                            {
+                                mode = "-e";
+                                path = args[0];
+                                break;
+                            }
+
+                            if (Directory.Exists(args[0]))
+                            {
+                                mode = "-i";
+                                path = args[0].TrimEnd('~');
+                            }
+
+                            break;
+                    }
+
+                    break;
+                case 2:
+                    _encoding = null;
+                    mode = args[0];
+                    path = args[1];
+                    break;
+                case 3:
+                    mode = args[0];
+                    path = args[1];
+                    _encoding = Encoding.GetEncoding(args[2]);
+                    break;
+            }
+
+            var scripts = Array.Empty<UnknownScript>();
+            switch (mode)
+            {
+                case "-e":
+                    _encoding ??= Encoding.GetEncoding("SHIFT-JIS");
+                    Console.WriteLine($"Read {Path.GetFullPath(path)}");
+                    using (var stream = File.OpenRead(path))
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        scripts = reader.ReadUnknownScripts();
+                    }
+
+                    Directory.CreateDirectory($"{path}~");
+
+                    foreach (var script in scripts)
+                    {
+                        if (!script.HasText()) continue;
+                        Console.WriteLine($"Export {script.Name}");
+                        using var writer = File.CreateText($"{path}~/{script.Name}.txt");
+                        for (var i = 0; i < script.Commands.Length; i++)
+                        {
+                            foreach (var line in Export(script.Commands[i]))
+                            {
+                                writer.WriteLine($">{BitConverter.ToUInt16(script.Commands[i], 0x00):X04}");
+                                writer.WriteLine($"◇{i:D4}◇{line}");
+                                writer.WriteLine($"◆{i:D4}◆{line}");
+                                writer.WriteLine();
+                            }
+                        }
+                    }
+
+                    break;
+                case "-i":
+                    _encoding ??= Encoding.GetEncoding("GBK");
+                    Console.WriteLine($"Read {Path.GetFullPath(path)}");
+                    var key = 0x0000_0000u;
+                    using (var stream = File.OpenRead(path))
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        scripts = reader.ReadUnknownScripts();
+                        stream.Position = 0x0000_0008;
+                        key |= reader.ReadUInt32();
+                    }
+
+                    foreach (var script in scripts)
+                    {
+                        if (!script.HasText()) continue;
+                        if (!File.Exists($"{path}~/{script.Name}.txt")) continue;
+                        Console.WriteLine($"Import {script.Name}");
+                        var translated = new string[script.Commands.Length][];
+                        foreach (var line in File.ReadLines($"{path}~/{script.Name}.txt"))
+                        {
+                            var m = Regex.Match(line, @"◆(\d+)◆(.+$)");
+                            if (!m.Success) continue;
+
+                            var index = int.Parse(m.Groups[1].Value);
+                            var text = m.Groups[2].Value;
+
+                            var lines = translated[index] ?? Array.Empty<string>();
+                            Array.Resize(ref lines, lines.Length + 1);
+                            lines[lines.Length - 1] = text;
+                            translated[index] = lines;
+                        }
+
+                        for (var i = 0; i < script.Commands.Length; i++)
+                        {
+                            if (translated[i] == null) continue;
+                            script.Commands[i] = Import(script.Commands[i], translated[i]);
+                        }
+                    }
+
+                    var filename = $"{Path.GetFileNameWithoutExtension(path)}_{_encoding.WebName}.aqa";
+                    Console.WriteLine($"Write {filename}");
+                    using (var stream = File.Create(filename))
+                    using (var writer = new BinaryWriter(stream))
+                    {
+                        writer.WriteUnknownScripts(scripts, key);
+                    }
+
+                    break;
+                default:
+                    Array.Resize(ref scripts, 0);
+                    Console.WriteLine("Usage:");
+                    Console.WriteLine("  Export text : UnknownScriptTool -e [ISF] [encoding]");
+                    Console.WriteLine("  Import text : UnknownScriptTool -i [ISF] [encoding]");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    return;
+            }
+        }
+
+        private static Encoding _encoding = Encoding.Default;
+
+        private const string FileHead = "AQA ";
+
+        private static UnknownScript[] ReadUnknownScripts(this BinaryReader reader)
+        {
+            var head = _encoding.GetString(reader.ReadBytes(0x04));
+            if (head != FileHead) throw new NotSupportedException($"Not supported version: {head}.");
+            _ = reader.ReadUInt32();
+            var key = reader.ReadUInt32();
+            var mask = (ushort)(((0x0065 * key + 0x0309) & 0xFFFF) + 0x0001);
+            var count = reader.ReadInt32();
+
+            reader.BaseStream.Position = 0x0000_0018;
+            var index = reader.ReadBytes(count * 0x90);
+            for (var i = 0; i < index.Length; i++)
+            {
+                index[i] ^= (byte)((i & 0x01) == 0x00 ? mask : mask >> 0x08);
+            }
+
+            var data = 0x0000_0018 + index.Length;
+            using var s = new MemoryStream(index);
+            using var r = new BinaryReader(s);
+            var scripts = new UnknownScript[count];
+            for (var i = 0; i < count; i++)
+            {
+                s.Position = i * 0x90;
+                var name = _encoding.GetString(r.ReadBytes(0x80).TrimEnd());
+                var size = r.ReadInt32();
+                var sort = r.ReadUInt32();
+                var offset = data + r.ReadUInt32();
+
+                reader.BaseStream.Position = offset;
+                var bytes = reader.ReadBytes(size);
+                scripts[i] = new UnknownScript(name, sort, bytes);
+            }
+
+            return scripts;
+        }
+
+        private static void WriteUnknownScripts(this BinaryWriter writer, UnknownScript[] scripts, uint key)
+        {
+            writer.Write(_encoding.GetBytes(FileHead));
+            writer.Write(0x0000_FFFF);
+            writer.Write(key);
+            writer.Write(scripts.Length);
+
+            var index = new byte[scripts.Length * 0x90];
+            using var s = new MemoryStream(index);
+            using var w = new BinaryWriter(s);
+            var data = 0x0000_0018 + index.Length;
+            writer.BaseStream.Position = data;
+            for (var i = 0; i < scripts.Length; i++)
+            {
+                var bytes = scripts[i].ToBytes();
+
+                s.Position = i * 0x90;
+                w.Write(_encoding.GetBytes(scripts[i].Name));
+                s.Position = i * 0x90 + 0x80;
+                w.Write(bytes.Length);
+                w.Write(scripts[i].Sort);
+                w.Write((uint)(writer.BaseStream.Position - data));
+
+                writer.Write(bytes);
+            }
+
+            var mask = (ushort)(((0x0065 * key + 0x0309) & 0xFFFF) + 0x0001);
+            for (var i = 0; i < index.Length; i++)
+            {
+                index[i] ^= (byte)((i & 0x01) == 0x00 ? mask : mask >> 0x08);
+            }
+
+            writer.BaseStream.Position = 0x0000_0018;
+            writer.Write(index);
+        }
+
+        private static string[] Export(byte[] command)
+        {
+            switch (BitConverter.ToUInt16(command, 0x00))
+            {
+                case 0x0201:
+                case 0x0241:
+                {
+                    var len = BitConverter.ToUInt16(command, 0x02) - 0x01;
+                    while (command[0x04 + len] != 0x00) len--;
+                    return _encoding.GetString(command, 0x04, len).Split('\n');
+                }
+                case 0x027A:
+                    var count = BitConverter.ToUInt32(command, 0x06) - 0x01;
+                    var arr = new string[count];
+                    var position = 0x0A;
+                    while (true)
+                    {
+                        switch (BitConverter.ToUInt16(command, position))
+                        {
+                            case 0x15:
+                            case 0x16:
+                            case 0x17:
+                            case 0x18:
+                            case 0x19:
+                                position += 0x06;
+                                continue;
+                        }
+
+                        break;
+                    }
+
+                    for (var i = 0; i < count; i++)
+                    {
+                        var len = BitConverter.ToUInt16(command, position + 0x02) - 0x01;
+                        while (command[position + 0x04 + len] != 0x00) len--;
+                        arr[i] = _encoding.GetString(command, 0x04, len);
+                        position += BitConverter.ToUInt16(command, position + 0x02) + 0x04;
+                    }
+
+                    return arr;
+                default:
+                    return Array.Empty<string>();
+            }
+        }
+
+        private static byte[] Import(byte[] command, string[] lines)
+        {
+            if (_encoding.CodePage == 936)
+            {
+                for (var i = 0; i < lines.Length; i++)
+                {
+                    lines[i] = lines[i].ReplaceGbkUnsupported();
+                }
+            }
+
+            switch (BitConverter.ToUInt16(command, 0x00))
+            {
+                case 0x0201:
+                case 0x0241:
+                {
+                    var bytes = _encoding.GetBytes(string.Join("\n", lines));
+                    var len = (bytes.Length + 0x04) & ~0x03;
+                    var buffer = new byte[0x04 + len];
+                    using var steam = new MemoryStream(buffer);
+                    using var writer = new BinaryWriter(steam);
+
+                    writer.Write(command, 0x00, 0x02);
+                    writer.Write((ushort)len);
+                    writer.Write(bytes);
+
+                    return buffer;
+                }
+                case 0x027A:
+                {
+                    var count = BitConverter.ToUInt32(command, 0x06) - 0x01;
+                    if (count != lines.Length) throw new FormatException("Invalid number of lines");
+                    var position = 0x0A;
+                    while (true)
+                    {
+                        switch (BitConverter.ToUInt16(command, position))
+                        {
+                            case 0x15:
+                            case 0x16:
+                            case 0x17:
+                            case 0x18:
+                            case 0x19:
+                                position += 0x06;
+                                continue;
+                        }
+
+                        break;
+                    }
+
+                    var buffer = new byte[position + lines.Sum(line => (_encoding.GetByteCount(line) + 0x08) & ~0x03)];
+
+                    using var steam = new MemoryStream(buffer);
+                    using var writer = new BinaryWriter(steam);
+
+                    writer.Write(command, 0x00, position);
+                    steam.Position = 0x0000_0002;
+                    writer.Write(buffer.Length);
+                    foreach (var line in lines)
+                    {
+                        steam.Position = position;
+                        var bytes = _encoding.GetBytes(line);
+                        var len = (bytes.Length + 0x04) & ~0x03;
+                        writer.Write((ushort)0x0201);
+                        writer.Write((ushort)len);
+                        writer.Write(bytes);
+                        position += 0x04 + len;
+                    }
+
+                    return buffer;
+                }
+                default:
+                    return command;
+            }
+        }
+
+        private static bool HasText(this UnknownScript script)
+        {
+            return script.Commands
+                .Select(command => BitConverter.ToUInt16(command, 0x00))
+                .Any(instruction => instruction == 0x0241 || instruction == 0x0201 || instruction == 0x027A);
+        }
+    }
+}
---
+++ UnknownScriptTool/UnknownScript.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Unknown
+{
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    public readonly struct UnknownScript
+    {
+        public readonly string Name;
+
+        public readonly uint Sort;
+
+        public readonly uint Key;
+
+        public readonly byte[][] Commands;
+
+        public UnknownScript(string name, uint sort, byte[] bytes)
+        {
+            Name = name;
+            Sort = sort;
+
+            var commands = new List<byte[]>();
+            using var steam = new MemoryStream(bytes);
+            using var reader = new BinaryReader(steam);
+            var header = reader.ReadUInt32();
+            if (header != 0x2054_5543) throw new FormatException($"header: {header:X8}");
+            var x04 = reader.ReadUInt32();
+            if (x04 != bytes.Length - 0x1C) throw new FormatException($"0x04: {x04:X8}");
+            Key = reader.ReadUInt32();
+            var x0C = reader.ReadUInt32();
+            if (x0C != 0x0001_0000) throw new FormatException($"0x0C: {x0C:X8}");
+
+            var mask = (byte)(0x75 * Key + 0x42);
+            for (var i = 0x1C; i < bytes.Length; i++) bytes[i] ^= mask;
+
+            steam.Position = 0x0000_001C;
+            while (steam.Position < bytes.Length)
+            {
+                var position = steam.Position;
+                var instruction = reader.ReadUInt16();
+                var size = 0x00;
+
+                switch (instruction)
+                {
+                    case 0x0001:
+                    case 0x0002:
+                    case 0x0003:
+                    case 0x0004:
+                    case 0x0005:
+                    case 0x000C:
+                        size += 0x02;
+                        break;
+                    case 0x000D:
+                        size += 0x06;
+                        break;
+                    case 0x000E:
+                    case 0x000F:
+                    case 0x0010:
+                    case 0x0011:
+                    case 0x0012:
+                    case 0x0013:
+                        size += 0x02;
+                        break;
+                    case 0x0015:
+                    case 0x0016:
+                    case 0x0017:
+                    case 0x0018:
+                    case 0x0019:
+                    case 0x001A:
+                    case 0x001B:
+                    case 0x001C:
+                    case 0x001D:
+                        size += 0x06;
+                        break;
+                    case 0x001E:
+                    case 0x001F:
+                        size += 0x02;
+                        break;
+                    case 0x0021:
+                        size += 0x06;
+                        break;
+                    case 0x0026:
+                        size += 0x0A;
+                        break;
+                    case 0x01F4:
+                        size += 0x04;
+                        break;
+                    case 0x0200:
+                    case 0x0201:
+                    {
+                        var len = reader.ReadUInt16();
+                        // var text = Encoding.GetEncoding(932).GetString(reader.ReadBytes(len));
+                        // Debug.WriteLine(text);
+                        size += len + 0x04;
+                    }
+                        break;
+                    case 0x023F:
+                        size += 0x06;
+                        break;
+                    case 0x0241:
+                    {
+                        var len = reader.ReadUInt16();
+                        // var text = Encoding.GetEncoding(932).GetString(reader.ReadBytes(len));
+                        // Debug.WriteLine(text);
+                        size += len + 0x04;
+                    }
+                        break;
+                    case 0x0242:
+                    case 0x024B:
+                    case 0x024C:
+                    case 0x024D:
+                    case 0x024E:
+                    case 0x024F:
+                    case 0x0252:
+                    case 0x0253:
+                    case 0x0256:
+                    case 0x0257:
+                    case 0x0258:
+                    case 0x025A:
+                    case 0x025B:
+                    case 0x025D:
+                    case 0x0262:
+                    case 0x0263:
+                    case 0x0265:
+                        size += 0x08;
+                        break;
+                    case 0x0268:
+                    case 0x0269:
+                    case 0x026A:
+                        size += 0x04;
+                        break;
+                    case 0x026D:
+                    case 0x026E:
+                        size += 0x06;
+                        break;
+                    case 0x0270:
+                    case 0x0271:
+                        size += 0x08;
+                        break;
+                    case 0x0272:
+                    case 0x0273:
+                    case 0x0274:
+                    case 0x0275:
+                        size += 0x04;
+                        break;
+                    case 0x0276:
+                        size += 0x06;
+                        break;
+                    case 0x0277:
+                    case 0x0279:
+                        size += 0x04;
+                        break;
+                    case 0x027A:
+                        size += (int)(reader.ReadUInt32() + 0x1C - position);
+                        break;
+                    case 0x027B:
+                        size += 0x02;
+                        break;
+                    default:
+                        throw new FormatException($"unknown instruction at {Name}:{position:X08} : {instruction:X04}");
+                }
+
+                steam.Position = position;
+                commands.Add(reader.ReadBytes(size));
+            }
+
+            Commands = commands.ToArray();
+        }
+
+        public byte[] ToBytes()
+        {
+            var bytes = new byte[0x0000_001C + Commands.Sum(command => command.Length)];
+            using var steam = new MemoryStream(bytes);
+            using var writer = new BinaryWriter(steam);
+
+            writer.Write(Encoding.ASCII.GetBytes("CUT "));
+            writer.Write((uint)(bytes.Length - 0x1C));
+            writer.Write(Key);
+            writer.Write(0x0001_0000);
+
+            steam.Position = 0x0000_001C;
+            foreach (var command in Commands) writer.Write(command);
+            
+            var mask = (byte)(0x75 * Key + 0x42);
+            for (var i = 0x1C; i < bytes.Length; i++) bytes[i] ^= mask;
+            
+            return bytes;
+        }
+    }
+}
