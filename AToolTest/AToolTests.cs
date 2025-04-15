2bb14df74f1874c1f1578d505954dfa4c50891db
cssxsh <cssxsh@gmail.com>
2025-04-15 10:41:51 +0800

    fix: AToolTests

---
+++ AToolTest/AToolTests.cs
@@ -0,0 +1,142 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace ATool
+{
+    [TestFixture]
+    internal static class AToolTests
+    {
+        private static Encoding _jis = Encoding.GetEncoding("SHIFT-JIS");
+        private static Encoding _gbk = Encoding.GetEncoding("GBK");
+
+        #region Ikura GDL
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void AOI()
+        {
+            // ...
+        }
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void SUME()
+        {
+            // ...
+        }
+
+        #endregion
+
+        #region SYSTEM-ε
+
+        [Test]
+        public static void AOI_RO()
+        {
+            // ...
+        }
+
+        #endregion
+
+        #region Will
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void SALEM()
+        {
+            // ...
+        }
+
+        [Test]
+        public static void SALEM_TRUE()
+        {
+            // ...
+        }
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void SLAVE_R()
+        {
+            // ...
+        }
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void MBF0()
+        {
+            // ...
+        }
+
+        #endregion
+
+        #region BGI/Ethornell
+
+        [Test]
+        public static void EVE_A()
+        {
+            // ...
+        }
+
+        #endregion
+
+        #region Unknown
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void ABANDONER()
+        {
+            // ...
+        }
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void NO_REALITY()
+        {
+            // ...
+        }
+
+        #endregion
+
+        #region TamaSoft
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void LOST_CHILD()
+        {
+            // ...
+        }
+
+        #endregion
+
+        #region Mutation
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void SEIRYAKU()
+        {
+            // ...
+        }
+
+        #endregion
+
+        #region CatSystem2
+
+        // ReSharper disable once InconsistentNaming
+        [Test]
+        public static void GRISAIA()
+        {
+            // ...
+        }
+
+        #endregion
+
+        #region rUGP
+
+        [Test]
+        public static void CSbm()
+        {
+            // ...
+        }
+
+        #endregion
+    }
+}
